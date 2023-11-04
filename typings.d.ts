c390a8429ba5e3bdd51ffa54eaab614056a5b769
vanishcode <vanishcode@outlook.com>
2023-11-04 21:55:33 +0800

    chore: add missing types

--- typings.d.ts
+++ typings.d.ts
@@ -2,9 +2,15 @@ import 'umi/typings';
 
 declare global {
   interface Window {
-    okxwallet: any;
     ethereum: any;
     tronWeb: any;
     keplr: any;
+    aptos: any;
+    sui: any;
+    solana: any;
+    bitcoin: any;
+    stacks: any;
+    starknet: any;
+    agentX: any;
   }
 }
