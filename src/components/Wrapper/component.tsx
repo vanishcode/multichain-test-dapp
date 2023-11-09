a706f5ba3ab4dd4ab3245a9d96063e04bbef3a7a
vanishcode <vanishcode@outlook.com>
2023-11-09 23:19:08 +0800

    refactor: Wrapper component

--- src/components/Wrapper/component.tsx
+++ src/components/Wrapper/component.tsx
@@ -1,17 +1,5 @@
-import { Card, Col } from 'antd';
+import { Row } from 'antd';
 
-export default function Wrapper({
-  children,
-  name,
-}: {
-  children?: React.ReactNode;
-  name: string;
-}) {
-  return (
-    <Col xs={24} sm={24} md={12} lg={12} xl={12} xxl={12}>
-      <Card title={name} style={{ margin: '16px 0' }}>
-        {children}
-      </Card>
-    </Col>
-  );
+export default function Wrapper({ children }: { children?: React.ReactNode }) {
+  return <Row gutter={16}>{children}</Row>;
 }
