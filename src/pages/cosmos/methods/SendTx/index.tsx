c93a5b1c447d6c86ec6b3d2628c94cd44ac37071
vanishcode <vanishcode@outlook.com>
2023-11-13 19:37:12 +0800

    feat(cosmos): support SendTx

--- src/pages/cosmos/methods/SendTx/index.tsx
+++ src/pages/cosmos/methods/SendTx/index.tsx
@@ -1,3 +1,29 @@
+import Item from '@/components/Item';
+import { TxRaw } from 'cosmjs-types/cosmos/tx/v1beta1/tx';
+
 export default function SendTx() {
-  return null;
+  const handleClick = async () => {
+    if (!window.signDirectResult) {
+      alert('need run signDirect first');
+      return;
+    }
+    const tx = TxRaw.encode({
+      bodyBytes: window.signDirectResult.signed.bodyBytes,
+      authInfoBytes: window.signDirectResult.signed.authInfoBytes,
+      signatures: [
+        Buffer.from(window.signDirectResult.signature.signature, 'base64'),
+      ],
+    }).finish();
+
+    const result = await window.keplr.sendTx(
+      window.signDirectResult.signed.chainId,
+      tx,
+      'async',
+    );
+    return result;
+  };
+
+  return (
+    <Item name="sendTx(need run signDirect first)" onClick={handleClick} />
+  );
 }
