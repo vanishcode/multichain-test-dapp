8ce55aabcfac4e1b224484665e3c9b50387def97
vanishcode <vanishcode@outlook.com>
2023-11-13 19:36:26 +0800

    feat(cosmos): add VerifyArbitrary

--- src/pages/cosmos/methods/VerifyArbitrary/index.tsx
+++ src/pages/cosmos/methods/VerifyArbitrary/index.tsx
@@ -1,3 +1,23 @@
+import Item from '@/components/Item';
+import { keplrConnect } from '@/utils/connect';
+
 export default function VerifyArbitrary() {
-  return null;
+  const value = {
+    chainId: 'injective-1',
+    message: 'Hello World',
+  };
+  const handleClick = async ({ chainId, message }: any) => {
+    await keplrConnect();
+    const { bech32Address: signer } = await window.keplr.getKey(chainId);
+    const signature = await window.keplr.verifyArbitrary(
+      chainId,
+      signer,
+      message,
+      // for test
+      window.signArbitraryResult,
+    );
+    return signature;
+  };
+
+  return <Item name="verifyArbitrary" value={value} onClick={handleClick} />;
 }
