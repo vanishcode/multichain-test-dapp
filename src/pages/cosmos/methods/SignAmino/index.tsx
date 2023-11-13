e99b912d55a5e33ce035f5a97530996de71dd7da
vanishcode <vanishcode@outlook.com>
2023-11-13 19:49:55 +0800

    feat(cosmos): SignArbitrary use true data

--- src/pages/cosmos/methods/SignAmino/index.tsx
+++ src/pages/cosmos/methods/SignAmino/index.tsx
@@ -1,56 +1,75 @@
 import Item from '@/components/Item';
 import { keplrConnect } from '@/utils/connect';
+import { useMount } from 'ahooks';
+import { useState } from 'react';
 
 export default function SignArbitrary() {
   const value = {
-    chainId: 'dydx-mainnet-1',
-    denom: 'adydx',
-    amount: '100000000000000000',
+    coinId: 21500,
+    chainId: 'injective-1',
+    denom: 'inj',
+    amount: '1200000000000000',
+    from: 'inj1ywqe8057srngat8rtz95tkx0ffl2urarkegcc8',
+    to: 'inj1ywqe8057srngat8rtz95tkx0ffl2urarkegcc8',
   };
 
-  const handleClick = async ({ chainId, denom, amount }: any) => {
+  const [feeDenom, setFeeDenom] = useState('inj');
+  const [feeAmount, setFeeAmount] = useState('180000000000000');
+  const [gasLimit, setGasLimit] = useState('200000');
+  const [accountNumber, setAccountNumber] = useState('630104');
+  const [sequence, setSequence] = useState('279');
+
+  useMount(() => {
+    keplrConnect();
+
+    fetch(
+      `https://wallet.okex.org/v1/deposit/plugin/coin/${value.coinId}/address/${value.to}/signInfo`,
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+
+        setAccountNumber(res.data.info.accountNumber);
+        setSequence(res.data.info.sequence);
+        setFeeDenom(res.data.info.feeDemon);
+        setFeeAmount(res.data.info.feeAmount);
+        setGasLimit(res.data.info.gasLimit);
+      });
+  });
+
+  const handleClick = async ({ chainId, denom, amount, from, to }: any) => {
     await keplrConnect();
+    await window.keplr.enable(chainId);
     const { bech32Address: signer } = await window.keplr.getKey(chainId);
-    const signature = await window.keplr.signAmino(
-      chainId,
-      signer,
-      {
-        account_number: '833360',
-        chain_id: chainId,
-        fee: {
-          gas: '174651',
-          amount: [
-            {
-              amount,
-              denom,
-            },
-          ],
-        },
-        memo: '',
-        msgs: [
-          {
-            type: 'sei/poolmanager/swap-exact-amount-in',
-            value: {
-              routes: [
-                {
-                  pool_id: '1',
-                  token_out_denom:
-                    'ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2',
-                },
-              ],
-              sender: signer,
-              token_in: {
-                amount: '10000',
-                denom,
-              },
-              token_out_min_amount: '545',
-            },
-          },
-        ],
-        sequence: '20',
+
+    const signDoc = {
+      chain_id: chainId,
+      account_number: accountNumber,
+      sequence,
+      fee: {
+        amount: [{ amount: feeAmount, denom: feeDenom }],
+        gas: gasLimit,
       },
-      { preferNoSetFee: true },
-    );
+      msgs: [
+        {
+          type: '/cosmos.bank.v1beta1.MsgSend',
+          value: {
+            from_address: from,
+            to_address: to,
+            amount: [
+              {
+                denom,
+                amount,
+              },
+            ],
+          },
+        },
+      ],
+      memo: 'sign amino',
+    };
+    const signature = await window.keplr.signAmino(chainId, signer, signDoc, {
+      preferNoSetFee: true,
+    });
     return signature;
   };
 
